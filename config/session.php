49d24859a96a420a94f873bc93c970efe7a0bd01
mendyx2 <wendyokarme@gmail.com>
2026-08-25 10:14:28 +0100

    Add cache and sessions migration tables and update default cache/session drivers to file

--- config/session.php
+++ config/session.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Str;
 
 return [
 
-    'default' => env('SESSION_DRIVER', 'database'),
+    'default' => env('SESSION_DRIVER', 'file'),
 
     'lifetime' => env('SESSION_LIFETIME', 120),
 
